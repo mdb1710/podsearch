8d2c8eeb84201edb0f32a229b0f6cc0ca7a48607
mdb1710 <mdb1710@gmail.com>
2020-06-09 23:20:05 -0400

    added clarity to searchform

--- src/app/search-podcast/search-podcast.component.ts
+++ src/app/search-podcast/search-podcast.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-search-podcast',
   templateUrl: './search-podcast.component.html',
   styleUrls: ['./search-podcast.component.scss']
 })
 export class SearchPodcastComponent implements OnInit {
+
+  searchForm = new FormGroup({
+    topic: new FormControl('', Validators.required)
+  })
+
+  onSubmit(){
+    console.log('form submitted');
+    const { topic } = this.searchForm.value;
+    console.log('topic is', topic)
+  }
 
   constructor() { }
 
